78c4f9b7e72fe29bf11daf947a123b645664408c
Knut Sælensminde <knut.saelensminde@drofus.com>
2022-03-07 15:21:29 +0100

    Added small sample project reproducing the error

---
+++ npgsql_ilrepack/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace npgsql_ilrepack
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.DispatcherUnhandledException += OnDispatcherUnhandledException;
+            
+            /*
+             * Manual type discovery that in the real scenario will be fed into the IOC container 
+             */
+            new TypeDiscoverer(new[] { typeof(App).Assembly });
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, e.Exception.GetType().Name);
+        }
+    }
+}
---
+++ npgsql_ilrepack/TypeDiscoverer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace npgsql_ilrepack
+{
+    internal class TypeDiscoverer
+    {
+        private List<Type> types = new List<Type>();
+        internal TypeDiscoverer(IEnumerable<Assembly> assemblies)
+        {
+            this.CollectTypes(assemblies);
+        }
+        private void CollectTypes(IEnumerable<Assembly> assemblies)
+        {
+            Parallel.ForEach(
+                assemblies,
+                assembly =>
+                {
+                    try
+                    {
+                        var typesInAssembly = assembly.GetTypes();
+                        this.types.AddRange(typesInAssembly);
+                    }
+                    catch (ReflectionTypeLoadException ex)
+                    {
+                        MessageBox.Show(
+                            $"Got {ex.LoaderExceptions.Length} loader exceptions. First message is: {ex.LoaderExceptions.First().Message}");
+                    }
+                });
+        }
+    }
+}
